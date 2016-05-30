a86d77cbc2e94fa72e271034ba501b767ef4fef7
Dean Liu <deanliu@gmail.com>
2016-05-30 15:06:43 -0700

    Comparing hyperparam update on 3-layer vs 5-layer network SGD + Momentum update

--- cs231n/assignment2/cs231n/optim.py
+++ cs231n/assignment2/cs231n/optim.py
@@ -65,7 +65,13 @@ def sgd_momentum(w, dw, config=None):
   # TODO: Implement the momentum update formula. Store the updated value in   #
   # the next_w variable. You should also use and update the velocity v.       #
   #############################################################################
-  pass
+  mu = config.get('momentum')
+  learning_rate = config.get('learning_rate')
+
+  v = mu * v - learning_rate * dw
+  w += v
+  next_w = w
+
   #############################################################################
   #                             END OF YOUR CODE                              #
   #############################################################################
